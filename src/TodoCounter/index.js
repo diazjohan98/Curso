daabc160ac37a4345b85da963862c52f10d02c6c
Johan Diaz <vasquez.johan@correounivalle.edu.co>
2022-12-21 17:12:09 -0500

    deploy v1

--- src/TodoCounter/index.js
+++ src/TodoCounter/index.js
@@ -5,7 +5,7 @@ import "./TodoCounter.css";
 function TodoCounter() {
   const {totalTodos, completedTodos} = React.useContext(TodoContext);
 
-  return <h2 className="TodoCounter">Has completado {completedTodos} de {totalTodos} TODOs</h2>;
+  return <h2 className="TodoCounter">Has completado {completedTodos} tareas de {totalTodos} TODOs</h2>;
 }
 
 export { TodoCounter };
